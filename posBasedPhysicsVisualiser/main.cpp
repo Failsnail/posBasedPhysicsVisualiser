cca4575279a752573f837c5ea8d986c7bd6fac79
Failsnail <koenmare@gmail.com>
2015-01-17 16:09:39 +0100

    starting up

--- posBasedPhysicsVisualiser/main.cpp
+++ posBasedPhysicsVisualiser/main.cpp
@@ -8,6 +8,7 @@
 #include <GLFW/glfw3.h>
 #include <glm/fwd.hpp>  //gebruik om glm sneller te laten compileren
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>     //glm gedeelte over matrix transformaties
 
 using namespace glm;
 
@@ -86,7 +87,7 @@ GLuint LoadShaders(const char * vertex_file_path,const char * fragment_file_path
     glDeleteShader(VertexShaderID);
     glDeleteShader(FragmentShaderID);
 
-    std::cout << "Shders succesfully initialized!" << std::endl;
+    std::cout << "Shaders succesfully initialized!" << std::endl;
 
     return ProgramID;
 }
@@ -177,6 +178,33 @@ int main() {
     glUseProgram(programID);
 
 
+
+    // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+    glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+    // Camera matrix
+    glm::mat4 View       = glm::lookAt(
+        glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+        glm::vec3(0,0,0), // and looks at the origin
+        glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+    );
+    // Model matrix : an identity matrix (model will be at the origin)
+    glm::mat4 Model      = glm::mat4(1.0f);  // Changes for each model !
+    // Our ModelViewProjection : multiplication of our 3 matrices
+    glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+
+
+    // Get a handle for our "MVP" uniform.
+    // Only at initialisation time.
+    GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+    // Send our transformation to the currently bound shader,
+    // in the "MVP" uniform
+    // For each model you render, since the MVP will be different (at least the M part)
+    glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+
+
                         //MAIN-LOOP
 
     while(!glfwWindowShouldClose(window)) {
@@ -209,5 +237,9 @@ int main() {
     }
 
     glfwTerminate();
+
+    std::string tempString;
+    std::cin >> tempString;
+
     return 0;
 }
