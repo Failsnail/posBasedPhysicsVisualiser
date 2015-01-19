72e0862fd11decff664973e25af1eb9f2dcbe891
Failsnail <koenmare@gmail.com>
2015-01-19 18:56:19 +0100

    bunch of important changes
    cube got replaced by starlike thingey!

--- posBasedPhysicsVisualiser/fragmentShader.glsl
+++ posBasedPhysicsVisualiser/fragmentShader.glsl
@@ -1,15 +1,13 @@
 #version 330 core
 
 // Interpolated values from the vertex shaders
-in vec3 fragmentColor;
+in float depth;
 
 // Ouput data
 out vec3 color;
 
-void main(){
+void main() {
 
-	// Output color = color specified in the vertex shader,
-	// interpolated between all 3 surrounding vertices
-	color = fragmentColor;
+	color = vec3(1,1,1) * (1 - depth / 15 + 0.1 / depth);
 
 }
